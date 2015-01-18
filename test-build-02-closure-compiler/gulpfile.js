ca8a49fd78d958b0f2cd60e3a5aeac26cd5be931
Lokis <lokis.git@gmail.com>
2015-01-18 20:08:52 +0100

    closure compiler - nefacha :-(

---
+++ test-build-02-closure-compiler/gulpfile.js
@@ -0,0 +1,96 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var clean = require('gulp-clean');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+var stylish = require('jshint-stylish');
+var checkstyleFileReporter = require('jshint-checkstyle-file-reporter');
+
+var htmlreplace = require('gulp-html-replace');
+var react = require('gulp-react');
+var closureCompiler = require('gulp-closure-compiler');
+
+
+process.env.JSHINT_CHECKSTYLE_FILE = 'jshint.xml'; // default: checkstyle.xml
+
+
+sources = {
+    js: {
+        my: './src/*.js',
+        jq: '../bower_components/jquery/jquery.js',
+        react: '../bower_components/react/react.js',
+        transformed: 'transformed.js',
+        compiled: 'compiled.js'
+    },
+    html: {
+        main: './src/index.html'
+    }
+};
+
+
+//gulp.task('build-js-my', ['build-js-transform'], function () {
+gulp.task('build-js-my', function () {
+    return gulp.src([sources.react, sources.js.transformed])
+        .pipe(closureCompiler({
+            compilerPath: '../bower_components/closure-compiler/lib/vendor/compiler.jar',
+            fileName: sources.js.compiled,
+            compilerFlags: {
+                closure_entry_point: 'app.main',
+                //compilation_level: 'ADVANCED_OPTIMIZATIONS',
+                define: [
+                    "goog.DEBUG=false"
+                ],
+                externs: [
+                    '../bower_components/react-externs/externs.js'
+                ],
+                extra_annotation_name: 'jsx',
+                // Some compiler flags (like --use_types_for_optimization) don't have value. Use null.
+                // use_types_for_optimization: null,
+                only_closure_dependencies: true,
+                output_wrapper: '(function(){%output%})();',
+                warning_level: 'VERBOSE'
+            }
+        }))
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('build-js-transform', ['clean'], function () {
+    return gulp.src(sources.js.my)
+        .pipe(react({harmony: true}))
+        .pipe(rename(sources.js.transformed))
+        .pipe(gulp.dest('.'));
+});
+
+
+gulp.task('build-html-main', ['clean'], function () {
+    return gulp.src(sources.html.main)
+        .pipe(htmlreplace({
+            'js-my': {
+                src: [sources.js.compiled],
+                tpl: '<script src="%s"></script>'
+            }
+        }))
+        .pipe(gulp.dest('.'))
+        .on('error', gutil.log)
+});
+
+gulp.task('lint', function () {
+    return gulp.src([sources.js.my])
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+        .pipe(jshint.reporter(checkstyleFileReporter));
+});
+
+
+gulp.task('clean', function () {
+    var src = ['jshint.xml', 'index.html', sources.js.compiled, sources.js.transformed];
+    return gulp.src(src, {read: false})
+        .pipe(clean());
+});
+
+
+gulp.task('build', ['build-html-main', 'build-js-transform']);
+
+gulp.task('compiler', ['build-js-my']);
+
+gulp.task('default', ['build']);
---
+++ test-build-02-closure-compiler/src/simple-todo-list.js
@@ -0,0 +1,47 @@
+var TodoList = React.createClass({
+    getInitialState: function () {
+        return {items: [], text: ''};
+    },
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var nextItems = this.state.items.concat([this.state.text]);
+        this.setState({ items: nextItems, text: '' });
+    },
+    onChange: function (e) {
+        this.setState({ text: e.target.value });
+    },
+    render: function () {
+        return (
+            <div>
+                <TodoListList list={ this.state.items } />
+                <form onSubmit={ this.handleSubmit } >
+                    <input onChange={ this.onChange } value={ this.state.text } />
+                    <button>{ ['Add #'], [this.state.items.length + 1] }</button>
+                </form>
+            </div>
+            );
+    }
+});
+
+var TodoListList = React.createClass({
+    render: function () {
+        var renderLi = function (i) {
+            return(
+                <li>{i}</li>
+                );
+        };
+        return (
+            <div>
+                <h2>Todo({ this.props.list.length }):</h2>
+                <ul>
+                { this.props.list.map(renderLi) }
+                </ul>
+            </div>
+            );
+    }
+});
+
+
+React.render(<TodoList />, document.getElementById('content'));
+React.render(<TodoList />, document.getElementById('content2'));
+
